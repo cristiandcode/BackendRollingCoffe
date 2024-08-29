65fe4fdc16e02a2216ae211d0599c54310ad0645
Cristian Diaz <diazcristian0210@gmail.com>
2024-08-29 16:43:06 -0300

    agregue la logica necesaria para crear un producto en la base de datos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,13 +1,23 @@
 import Producto from "../database/models/producto.js"
 
-export const listarProductos = (req, res)=>{
+export const listarProductos = async (req, res)=>{
     console.log("prueba de solicitud get")
     //enviar una respuesta
     res.send("desde mi backend de rolling coffee")
     }
-export const crearProducto = (req, res)=>{
-//Validar los datos del producto -- Estos datos vienen del body
+export const crearProducto = async (req, res)=>{
+    try{
+ //Validar los datos del producto -- Estos datos vienen del body
 //le vamos a pedir a la base de datos crear el producto
-console.log(req.body);
+ console.log(req.body);
+ const productoNuevo = new Producto(req.body)
+ await productoNuevo.save();
 //enviar la respuesta de lo sucedido, si funcionó o falló
+    res.status(201).json({mensaje: "El producto fue creado correctamente"})
+    }catch(error){
+        console.error(error)
+        res.status(500) .json({
+            mensaje: "Ocurrio un error y no se pudo crear el producto."
+        });
+    }
 };
