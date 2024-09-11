9ea13e864464ed330a61a09c08124d8036ce232b
Cristian Diaz <diazcristian0210@gmail.com>
2024-09-11 11:21:16 -0300

    agregue validaciones con express validator para el precio

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -18,8 +18,25 @@ router
       check("nombreProducto")
         .notEmpty()
         .withMessage("El nombre del producto es obligatorio")
-        .isLength({min: 2, max: 50})
-        .withMessage("El nombre del producto debe tener entre 2 y 50 caracteres inclusive")
+        .isLength({ min: 2, max: 50 })
+        .withMessage(
+          "El nombre del producto debe tener entre 2 y 50 caracteres inclusive"
+        ),
+      check("precio")
+        .notEmpty()
+        .withMessage("El precio del producto es un dato obligatorio")
+        .isNumeric()
+        .withMessage("El precio debe ser un número")
+        .custom((value) => {
+          if (value >= 50 && value <= 20000) {
+            return true;
+          } else {
+            throw new Error(
+              "El precio del producto debe estar entre $50 y $20000 inclusive"
+            );
+          }
+        }),
+
     ],
     crearProducto
   )
