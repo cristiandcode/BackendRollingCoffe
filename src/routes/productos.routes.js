81ccac8ee128208aba8553471a656f3a36daaa45
Cristian Diaz <diazcristian0210@gmail.com>
2024-09-10 22:41:08 -0300

    agregue validaciones xon express valdiator

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -13,7 +13,16 @@ check;
 router.route("/prueba").get(listarProductos);
 router
   .route("/productos")
-  .post([check("nombreProducto").notEmpty()], crearProducto)
+  .post(
+    [
+      check("nombreProducto")
+        .notEmpty()
+        .withMessage("El nombre del producto es obligatorio")
+        .isLength({min: 2, max: 50})
+        .withMessage("El nombre del producto debe tener entre 2 y 50 caracteres inclusive")
+    ],
+    crearProducto
+  )
   .get(listarProductos);
 router
   .route("/productos/:id")
